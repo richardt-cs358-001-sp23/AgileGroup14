c554e2e581789d3950c604bea31fea5bb9f939a9
JacobTL11 <jtlibertoski@gmail.com>
2023-04-27 17:55:18 -0500

    Updating reading from file to match the expected file format, also projectAdd and basic student PDO list

--- projectAdd.php
+++ projectAdd.php
@@ -5,7 +5,7 @@ $password = "pwd14";
 $dbname = "AgileExpG3"; 
 
 try {
-    $students_file = $_POST['studentsFile'];
+    $students_file = $_POST['projectsFile'];
     // Splitting of file migth change based on format of file
     $rows = explode("\n", $students_file);
     array_shift($rows)
@@ -16,21 +16,19 @@ try {
 
     // change to correct table and columns when those are created
     // not sure about inserting id yet have to deal with duplicates (insert might just deal with it)
-    $statement = $conn->prepare("INSERT INTO student (projectId, studentId, firstName, lastName, email) VALUES (?, ?, ?, ?, ?);");
+    $statement = $conn->prepare("INSERT INTO project (semesterId, projectId, projectName, projectDescription) VALUES (?, ?, ?, ?);");
 
-    // Split up the file into an array of students
-    $students = array()
+    // Split up the file into an array of students and add them to the database
     foreach($rows as $row => $data)
     {
         $row_data = explode(",", $data);
-        $student[$row]['projectId'] = $row_data[0];
-        $student[$row]['studentId'] = $row_data[1];
-        $student[$row]['firstname'] = $row_data[2];
-        $student[$row]['lastname']  = $row_data[3];
-        $student[$row]['email']     = $row_data[4];
+        $project[$row]['semesterID'] = $row_data[0];
+        $project[$row]['projectID']  = $row_data[1];
+        $project[$row]['projectname']  = $row_data[2];
+        $project[$row]['projectdescription']  = $row_data[3];
 
-        $statement->bind_param("iisss", $student[$row]['projectId'], $student[$row]['studentId'], $student[$row]['firstname'], $student[$row]['lastName'], $student[$row]['email']);
-        $statement->execute();
+        $project->bind_param("iiss", $project[$row]['semesterID'], $project[$row]['projectID'], $project[$row]['projectname'], $project[$row]['projectdescription']);
+        $project->execute();
     }
 }
 catch(PDOException $e) {
