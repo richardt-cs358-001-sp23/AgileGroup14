454171c2f0b3b4b3a45493b66ae42c63720eee8c
JacobTL11 <jtlibertoski@gmail.com>
2023-05-01 19:31:13 -0500

    Adding connection from project to student in worksOn table from file upload, also comments and changed error messages

--- studentAdd.php
+++ studentAdd.php
@@ -6,6 +6,7 @@ $dbname = "AgileExpG3";
 $data = array();
 
 try {
+    // get the name of the file that was posted to the server
     $studentsFile = $_FILES['filename']['tmp_name'];
     // Prepare the sql 
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password); 
@@ -13,7 +14,8 @@ try {
     // change to correct table and columns when those are created 
     // not sure about inserting id yet have to deal with duplicates (insert might just deal with it) 
     $insertStudent = $conn->prepare("INSERT INTO student (firstName, lastName, email) VALUES (:firstName, :lastName, :email);"); 
-    //$connectWorksOn = $conn->prepare("INSERT INTO worksOn (projectId, studentId) VALUES (:projectId, :studentId);");
+    $getInsertedId = $conn->prepare("SELECT studentId FROM student WHERE firstName = :firstName AND lastName = :lastName AND email = :email LIMIT 1"); 
+    $connectWorksOn = $conn->prepare("INSERT INTO worksOn (projectId, studentId) VALUES (:projectId, :studentId);");
 
     // Splitting of file might change based on format of file 
     if (($open = fopen($studentsFile, "r")) !== FALSE) 
@@ -21,23 +23,36 @@ try {
         fgetcsv($open, 1000, ",");
         while (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
         {        
+            // Insert the student into the database
             $insertStudent->bindParam(':firstName', $data[2], PDO::PARAM_STR, strlen($data[2]));
             $insertStudent->bindParam(':lastName', $data[3], PDO::PARAM_STR, strlen($data[3]));
             $insertStudent->bindParam(':email', $data[4], PDO::PARAM_STR, strlen($data[4]));
 
-            $insertStudent->execute();
+            if ($insertStudent->execute())
+            {
+                // Get the Id of the newly inserted Student
+                $getInsertedId->bindParam(':firstName', $data[2], PDO::PARAM_STR, strlen($data[2]));
+                $getInsertedId->bindParam(':lastName', $data[3], PDO::PARAM_STR, strlen($data[3]));
+                $getInsertedId->bindParam(':email', $data[4], PDO::PARAM_STR, strlen($data[4]));
 
-            //$connectWorksOn->bind_param("ii", $row_data[0], $row_data[1]);
-            //$connectWorksOn->execute();
+                if ($getInsertedId->execute())
+                {
+                    $Id = $getInsertedId->fetch()[0];
+                
+                    // Link the student to a project in the worksOn table
+                    $connectWorksOn->bindParam(':projectId', $data[0], PDO::PARAM_INT);
+                    $connectWorksOn->bindParam(':studentId', $Id, PDO::PARAM_INT);
+                    $connectWorksOn->execute();
+                }
+            }
         }
     
         fclose($open);
     }
     else
     {
-        echo "Failed to open file";
-        echo "Filename = $studentsFile";
-	print_r($_FILES);
+        echo "Error Opening file";
+        print_r($_FILES);
     }
 
 } 
